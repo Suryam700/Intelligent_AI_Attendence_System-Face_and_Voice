c42d78302d1ab81a4af07129f742e6d7c44fbfc7
Suryam Gahoi <suryamgahoi14@gmail.com>
2026-07-13 11:43:10 +0530

    adding some functionality on home screen hahaha

---
+++ src/components/footer.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def footer_home():
+    st.markdown("""
+                <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+                    <p style="font-weight:bold; color:white;"> Created with ❤️ by <a href="https://github.com/Suryam700" style="color: #fff; text-decoration: none;">Suryam Gahoi</a></p>  
+                </div>
+                """, unsafe_allow_html=True)
--- src/components/header.py
+++ src/components/header.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
 def header_home():
-    st.header("Snap Class")
+    logo_url = "https://i.ibb.co/YTYGn5qV/logo.png"
+    st.markdown(f"""
+                <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; margin: 30px 0;">
+                    <img src={logo_url} alt="App logo" style='height: 100px;'/>
+                    <h1 style='text-align: center; color: #E0E3FF'>SNAP<br/>CLASS</h1>
+                </div>
+                """, unsafe_allow_html=True)
