7a8b83f143578da7628f63fcb5861ad54b09d023
Suryam Gahoi <suryamgahoi14@gmail.com>
2026-07-19 21:17:31 +0530

    changes in student login interface

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -4,6 +4,8 @@ from src.components.footer import footer
 from src.UI.base_layout import style_background_dashboard, style_base_layout
 import numpy as np
 from PIL import Image
+from src.pipelines.face_pipeline import predict_attendance
+
 
 def student_screen():
     style_background_dashboard()
@@ -23,6 +25,20 @@ def student_screen():
 
     photo_source = st.camera_input("Position your Face in the center")
     if photo_source:
-        np.array(Image.open(photo_source))
+        img = np.array(Image.open(photo_source))
+
+        with st.spinner("AI is scanning..."):
+            detected, all_ids, num_faces = predict_attendance(img)
+
+            if num_faces == 0:
+                st.warning("No face found!")
+            elif num_faces > 1:
+                st.warning("Multiple faces Found!")
+            else:
+                if detected:
+                    student_id = list(detected.key())[0]
+                    
+                else:
+                    pass
 
     footer("#000")
